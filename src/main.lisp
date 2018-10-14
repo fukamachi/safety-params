bc502e37022ab10c079dcc9abe4721e0305ab41c
Eitaro Fukamachi <e.arrows@gmail.com>
2018-10-14 16:37:28 +0900

    Add 'validate'.

--- src/main.lisp
+++ src/main.lisp
@@ -7,8 +7,12 @@
   (:use-reexport #:safety-params/conversions
                  #:safety-params/assertions
                  #:safety-params/errors)
-  (:export #:sanitize))
+  (:export #:validate
+           #:sanitize))
 (in-package #:safety-params)
+
+(defmacro validate (form params)
+  `(nth-value 1 (funcall ,form ,params)))
 
 (defmacro sanitize (form params)
   `(handler-bind ((safety-params-error
@@ -16,4 +20,4 @@
                       (let ((restart (find-restart 'ignore-and-continue e)))
                         (when restart
                           (invoke-restart restart))))))
-     (funcall ,form ,params)))
+     (validate ,form ,params)))
